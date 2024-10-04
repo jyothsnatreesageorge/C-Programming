4b060199ddadf4c2b57f46dac1fc7cb52e8f195f
Jyothsna Treesa George <168282797+jyothsnatreesageorge@users.noreply.github.com>
2024-10-04 19:25:19 +0530

    Create Perimeter of rectangle.c

---
+++ Perimeter of rectangle.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main()
+{
+    int a,b,perimeter;
+    printf("Enter the sides of the recatngle:");
+    scanf("%d\n%d",&a,&b);
+    perimeter=2*(a+b);
+    printf("%d",perimeter);
+    return 0;
+}
